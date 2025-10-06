705e182e73418d3738a8506d835125f7cb986357
VinnieVanG0 <vincemai@umich.edu>
2025-10-06 13:19:40 -0400

    Update Pack.cpp

--- Pack.cpp
+++ Pack.cpp
@@ -3,18 +3,6 @@
 
 
 
-
-
-
-
-
-// EFFECTS: Initializes the Pack to be in the following standard order:
-  //          the cards of the lowest suit arranged from lowest rank to
-  //          highest rank, followed by the cards of the next lowest suit
-  //          in order from lowest to highest rank, and so on. 
-  // NOTE: The standard order is the same as that in pack.in.
-  // NOTE: Do NOT use pack.in in your implementation of this function
-  // NOTE: The pack is initially full, with no cards dealt.
   Pack::Pack(){
     int pack_index = 0;
     
@@ -27,19 +15,15 @@
     next = 0;
   }
 
-  // REQUIRES: pack_input contains a representation of a Pack in the
-  //           format required by the project specification
-  // MODIFIES: pack_input
-  // EFFECTS: Initializes Pack by reading from pack_input.
-  // NOTE: The pack is initially full, with no cards dealt.
-  Pack::Pack(std::istream& pack_input){// SOMETHING WRONG
+
+  Pack::Pack(std::istream& pack_input){
     int pack_index = 0;
     std::string in_rank;
     std::string in_suit;
     std::string in_junk;
     for(int suit = SPADES; suit <= DIAMONDS; suit++){
         for(int rank = NINE; rank <= ACE; rank++){
-            pack_input >> in_rank >> in_suit;
+            pack_input >> in_rank >> in_junk >> in_suit;
             cards[pack_index++] =  Card((string_to_rank(in_rank)),
                                         (string_to_suit(in_suit)));
         }
@@ -47,21 +31,16 @@
     next = 0;
   }
 
-  // REQUIRES: cards remain in the Pack
-  // EFFECTS: Returns the next card in the pack and increments the next index
+
   Card Pack::deal_one(){
        return cards[next++];
        
   }
 
-  // EFFECTS: Resets next index to first card in the Pack
   void Pack::reset(){
         next = 0;
   }
 
-  // EFFECTS: Shuffles the Pack and resets the next index. This
-  //          performs an in shuffle seven times. See
-  //          https://en.wikipedia.org/wiki/In_shuffle.
   void Pack::shuffle(){
     
     std::array<Card, PACK_SIZE> temp_cards;
@@ -84,7 +63,6 @@
     next = 0; // reset the index after shuffling
 }
 
-  // EFFECTS: returns true if there are no more cards left in the pack
   bool Pack::empty() const{
         return next >= PACK_SIZE;     
 
